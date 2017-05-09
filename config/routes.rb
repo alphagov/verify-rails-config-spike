cf32fafb9837424802e4c16324a72cb35179c7d2
William Palmer <william.palmer@digital.cabinet-office.gov.uk>
2017-05-09 09:54:02 +0100

    add routes required for receiving an authnrequest

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'config/index'
+
+  resources :idps, path: '/config/idps' do
+    resources :idp_display_data, path: '/display-data', only: [:index]
+  end
+  resources :transactions, path: '/config/transactions' do
+    resources :transactions_display_data, path: '/display-data', only: [:index]
+    resources :transactions_levels_of_assurance, path: '/levels-of-assurance', only: [:index]
+    resources :transactions_assertion_consumer_service_uri, path: '/assertion-consumer-service-uri', only: [:index]
+    resources :transactions_eidas_enabled, path: '/eidas_enabled', only: [:index]
+  end
+  resources :certificates, path: '/config/certificates' do
+    resources :certificates_signing, path: '/certs/signing', only: [:index]
+  end
+
+  root 'config#index'
 end
